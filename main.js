285b8b2b3aa6c5e91823a34cbab2a12f7e3fb5fe
nile1 <ng9454@gmail.com>
2020-03-12 01:52:03 +0530

    Version 2

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World!! \n Simplilearn Devops Certification submission by Nilesh Gupta, Version1 ");
+  response.end("Hello World!! \n Simplilearn Devops Certification submission by Nilesh Gupta, Version 2  ");
 });
 
 // listen on localhost:8000
